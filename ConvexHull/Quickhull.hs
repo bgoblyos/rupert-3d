57a0576c4b184837f86221ade0972fd5f3059906
unit511 <goblyos.bence@yahoo.com>
2022-07-08 18:23:39 +0200

    Fix 2D quickhull order

--- ConvexHull/Quickhull.hs
+++ ConvexHull/Quickhull.hs
@@ -15,7 +15,7 @@ quickHull2d xs = [a] ++ findHull set2 b a ++ [b] ++ findHull set1 a b
         set2 = filter (isRight b a) xs
 
 findHull [] _ _ = []
-findHull xs a b = [c] ++ findHull set1 a c ++ findHull set2 c b
+findHull xs a b = findHull set2 c b ++ [c] ++ findHull set1 a c
   where c = maxDist a b xs
         outside = filter (not . isInside a b c) xs
         set1 = filter (isRight a c) outside
