cf35ca02c8caa0c92fa468df60437b56b4f316a0
unit511 <goblyos.bence@yahoo.com>
2022-07-18 20:26:05 +0200

    Revert "Set up cabal for managing depedencies and progress on PRNG"
    This reverts commit 785a6aaee8c2ab5449964c8058c1e5e9df4c1193.

--- src/Main.hs
+++
@@ -1,9 +0,0 @@
-import Plot
-import Quickhull
-
-main = do
-  plotHull points hull
-
-points = [[0,0], [1,1], [5,6], [5,8], [-7,9], [-3,-7], [-5,3], [-2,4], [3,3], [-1,6], [5,5], [4,-4], [0.5, 0.7], [5.7, 3.5]]
-hull = quickHull2d points
-
